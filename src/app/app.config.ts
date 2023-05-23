f2228d5c9895546cbdfd33c1199a7cedb6faa195
Baruch Giverts <bor770@gmail.com>
2023-05-23 17:48:40 +0300

    beginning setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideAnimations(),
+    provideEffects(),
+    provideRouterStore(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
